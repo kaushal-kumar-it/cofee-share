f7bfeee8e2fd0c62ae92e23d97d18b65e750a95f
kaushal-kumar-it <techs308@gmail.com>
2025-09-13 23:26:00 +0530

    added analytics

--- forntend/src/App.tsx
+++ forntend/src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import { Analytics } from "@vercel/analytics/react"
 import Upload from "./Upload";
 import NotFound from "./pages/NotFound";
 //import Upload from "./pages/Uplaod1";
@@ -15,6 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
@@ -25,6 +27,7 @@ const App = () => (
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
+
 );
 
 export default App;
